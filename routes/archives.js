710f779f8954a25e8b125e935578cb6130dd615f
PlainviewData <dbalagula@nyu.edu>
2017-03-14 00:41:24 -0400

    Update archives.js

--- routes/archives.js
+++ routes/archives.js
@@ -20,6 +20,7 @@ router.get('/:archive_id', function(req,res){
 
 router.get('/a/:archive_id', function(req,res){
 	archiveTools.getArchiveById(req.params.archive_id, function(archive){
+		console.log(archive);
 		articleTools.getArticleById(archive.article, function(article){
 			console.log(article);
 			responseHandler.handleResponse({archive: archive, article: article}, res);
